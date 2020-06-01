6be925f6b60748c68e5cbe01d4c59f83f098472d
ShuvaDev <s.r.shuvadeb@gmail.com>
2020-06-02 00:38:39 +0600

    Updates

---
+++ precache-manifest.ea1f86899b351aa58dd5ff0176b4b392.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77d6639452eb968fbb9689bbbf2d4157",
+    "url": "/React-Todo-App/index.html"
+  },
+  {
+    "revision": "e7558bf964603279ecc5",
+    "url": "/React-Todo-App/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "e7558bf964603279ecc5",
+    "url": "/React-Todo-App/static/js/2.c2e5ded3.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/React-Todo-App/static/js/2.c2e5ded3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b5431a57bfefc25e09c",
+    "url": "/React-Todo-App/static/js/main.289e88fc.chunk.js"
+  },
+  {
+    "revision": "7a60793e896cbcd66f50",
+    "url": "/React-Todo-App/static/js/runtime-main.d6450314.js"
+  }
+]);
